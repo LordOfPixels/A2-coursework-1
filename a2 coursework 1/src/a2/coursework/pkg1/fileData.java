da80b3cb28efb6411a00ecefd6714d006125415c
Viv Barton <viv@trashcompactor.co.uk>
2014-07-26 17:20:15 +0100

    Initial upload

---
+++ a2 coursework 1/src/a2/coursework/pkg1/fileData.java
@@ -0,0 +1,29 @@
+package a2.coursework.pkg1;
+import java.io.IOException;
+
+public class fileData {
+    
+    
+    public static void main(String[] args)throws IOException {
+        
+        String fileName = "C:/git/a2-coursework-1/log.txt";
+        System.out.println("Run Main");
+        String[] allLines;
+        try {            
+            System.out.println("Succesfully tried");
+            readFile file = new readFile(fileName);
+            allLines = file.openFile();
+            for (String allLine : allLines) {
+                System.out.println(allLine);
+            }
+        
+            for (String allLine : allLines) {    
+            }
+                        
+        }
+        catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        
+    }
+}
---
+++ a2 coursework 1/src/a2/coursework/pkg1/readFile.java
@@ -0,0 +1,67 @@
+package a2.coursework.pkg1;
+import java.io.IOException;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.util.ArrayList;
+
+public class readFile {
+    
+    private String path;
+    
+    public readFile(String filePath){
+        path = filePath;
+        System.out.println("path declared, path: " + path);
+    }
+    
+    public String[] openFile() throws IOException{
+        
+        
+        FileReader fr = new FileReader(path);
+        BufferedReader textReader = new BufferedReader(fr);
+        System.out.println("file opened");
+        
+        int numLines = readLines();
+        String tempWord ="";
+        String tempLine;
+        ArrayList<String> textArray = new ArrayList<String>();
+        
+        int i;
+        int k = 0;
+        for (i=0; i < numLines; i++) {
+            System.out.println("loop #" + i);
+            tempLine = textReader.readLine();
+            for (int j = 0; j < tempLine.length(); j++){
+            if (tempLine.charAt(j) != '-'){
+                tempWord = tempWord + tempLine.charAt(j);                
+            }
+            else{
+                textArray.add(tempWord);
+                tempWord = "";
+                k++;
+            }
+        }        
+    }
+    
+    textReader.close();
+    
+    String[] textData = textArray.toArray(new String[0]);
+    return textData;
+    
+
+}
+    int readLines() throws IOException {
+        System.out.println("readLines began");
+        FileReader fileToRead = new FileReader (path);
+        BufferedReader br = new BufferedReader(fileToRead);
+        
+        String aLine;
+        int numberOfLines = 0;
+        
+        while ((aLine = br.readLine()) != null){
+            numberOfLines++;
+        }
+        br.close();
+        
+        return numberOfLines;
+    }
+}
