295bd035fcd7789898943b3f44f33514394a1e9b
Viv Barton <viv@trashcompactor.co.uk>
2014-08-21 22:01:30 +0100

    Fixed the file IO issue

--- a2 coursework 1/src/a2/coursework/pkg1/fileData.java
+++ a2 coursework 1/src/a2/coursework/pkg1/fileData.java
@@ -6,11 +6,11 @@ public class fileData {
     
     public static void main(String[] args)throws IOException {
         
-        String fileName = "C:\\log\\log.txt";
+        String fileName = "C:\\git\\a2-coursework-1\\log.log";
         System.out.println("Run Main");
         String[] allLines;
         try {
-            System.out.println("Succesfully tried");
+            System.out.println("Succesfully  tried");
             readFile file = new readFile(fileName);
             allLines = file.openFile();
             for (String allLine : allLines) {
